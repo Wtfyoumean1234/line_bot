1848522ded1d5f6cc75b7def493c7ccf8e8a70b1
Wtfyoumean1234 <133093541+Wtfyoumean1234@users.noreply.github.com>
2025-12-27 09:57:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -281,5 +281,5 @@ if __name__ == "__main__":
                             })
     sche.start()
     atexit.register(lambda: sche.shutdown())
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
